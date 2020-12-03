9d8e5ec3c29a3304dc46a1b23a138824f88bfccf
Justin Phelps <justin.phelps@dominodatalab.com>
2020-12-03 10:04:12 -0700

    saving database after creation and reload it.

--- beta_start.sh
+++ beta_start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-#this is a test to see if Docker does not suck. 
-
 #Check if Airflow Dir is present in Domino Project....if not create airflow directory
 if [ ! -d $DOMINO_WORKING_DIR/airflow ]; then
 	echo "Creating Airflow Directory"
@@ -47,22 +44,40 @@ if [ ! -d $DOMINO_WORKING_DIR/airflow ]; then
 	#Catchup by default
 	sed -i '639s#True#False#' "$DOMINO_WORKING_DIR"/airflow/airflow.cfg
 
+	#Create Database and copy it to domino working dir. 
+	#Create DB and user
+	sudo chown -R postgres /mnt/airflow/postgresql/
+	sudo service postgresql start
+	echo "CREATE USER airflow with PASSWORD 'airflow'" | sudo sh -c 'sudo -u postgres psql'
+	echo "CREATE DATABASE airflow;" | sudo sh -c 'sudo -u postgres psql'
+	echo "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO airflow;" | sudo sh -c 'sudo -u postgres psql'
+	#link new config file 
+	rm /home/ubuntu/airflow/airflow.cfg && ln -s /mnt/airflow/airflow.cfg /home/ubuntu/airflow/airflow.c
+	#build database and add env vars. 
+	airflow initdb
+	airflow variables -s DOMINO_API_HOST $DOMINO_API_HOST
+	airflow variables -s DOMINO_USER_API_KEY $DOMINO_USER_API_KEY
+	#backup airflow db
+	echo pg_dump --format=c airflow > /mnt/airflow/postgresql/db_dump_file.dump | sudo sh -c 'sudo -u postgres psql'
+
 fi
 
 #create DB in postgres
 sudo chown -R postgres /mnt/airflow/postgresql/
 sudo service postgresql start
 
-echo "CREATE USER airflow with PASSWORD 'airflow'" | sudo sh -c 'sudo -u postgres psql'
-echo "CREATE DATABASE airflow;" | sudo sh -c 'sudo -u postgres psql'
-echo "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO airflow;" | sudo sh -c 'sudo -u postgres psql'
+# echo "CREATE USER airflow with PASSWORD 'airflow'" | sudo sh -c 'sudo -u postgres psql'
+# echo "CREATE DATABASE airflow;" | sudo sh -c 'sudo -u postgres psql'
+# echo "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO airflow;" | sudo sh -c 'sudo -u postgres psql'
+echo pg_restore -v -d airflow /mnt/airflow/postgresql/db_dump_file.dump | sudo sh -c 'sudo -u postgres psql'
+
 
 echo "build dependencies"
 #Create symbolic link and remove default file
 sudo ln -s $DOMINO_WORKING_DIR/airflow/airflow.cfg /home/ubuntu/airflow/airflow.cfg
-airflow initdb
-airflow variables -s DOMINO_API_HOST $DOMINO_API_HOST
-airflow variables -s DOMINO_USER_API_KEY $DOMINO_USER_API_KEY
+# airflow initdb
+# airflow variables -s DOMINO_API_HOST $DOMINO_API_HOST
+# airflow variables -s DOMINO_USER_API_KEY $DOMINO_USER_API_KEY
 #start airflow webserver and scheduler
 echo "Starting up Airflow"
 airflow webserver -p 8080 -hn "0.0.0.0" &
