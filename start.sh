18e88a19c4a7bb95055852f2319083b679e9baed
Justin Phelps <justin.phelps@dominodatalab.com>
2020-12-22 08:09:49 -0700

    balh

--- start.sh
+++ start.sh
@@ -58,18 +58,18 @@ if [ ! -d $DOMINO_WORKING_DIR/airflow ]; then
 	airflow variables -s DOMINO_API_HOST $DOMINO_API_HOST
 	airflow variables -s DOMINO_USER_API_KEY $DOMINO_USER_API_KEY
 	#backup airflow db
-	sleep 5
-	sudo sh -c 'sudo -u postgres pg_dumpall --file=/mnt/airflow/postgresql/dumpall.sql'
+	# sleep 5
+	# sudo sh -c 'sudo -u postgres pg_dumpall --file=/mnt/airflow/postgresql/dumpall.sql'
 fi
 
 #create DB in postgres
 sudo chown -R postgres /mnt/airflow/postgresql/
 sudo service postgresql start
-#echo "CREATE USER airflow with PASSWORD 'airflow'" | sudo sh -c 'sudo -u postgres psql'
-#echo "CREATE DATABASE airflow;" | sudo sh -c 'sudo -u postgres psql'i
-#echo "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO airflow;" | sudo sh -c 'sudo -u postgres psql'
-echo "Restore Database"
-sudo sh -c 'sudo -u postgres psql -f /mnt/airflow/postgresql/dumpall.sql'
+echo "CREATE USER airflow with PASSWORD 'airflow'" | sudo sh -c 'sudo -u postgres psql'
+echo "CREATE DATABASE airflow;" | sudo sh -c 'sudo -u postgres psql'i
+echo "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO airflow;" | sudo sh -c 'sudo -u postgres psql'
+# echo "Restore Database"
+# sudo sh -c 'sudo -u postgres psql -f /mnt/airflow/postgresql/dumpall.sql'
 echo "link custom airflow.cfg"
 #Create symbolic link and remove default file
 FILE=/home/ubutu/airflow/airflow.cfg
@@ -78,9 +78,9 @@ if [ -f /home/ubuntu/airflow/airflow.cfg ]; then
         rm  /home/ubuntu/airflow/airflow.cfg
 fi
 sudo ln -s $DOMINO_WORKING_DIR/airflow/airflow.cfg /home/ubuntu/airflow/airflow.cfg
-# airflow initdb
-# airflow variables -s DOMINO_API_HOST $DOMINO_API_HOST
-# airflow variables -s DOMINO_USER_API_KEY $DOMINO_USER_API_KEY
+airflow initdb
+airflow variables -s DOMINO_API_HOST $DOMINO_API_HOST
+airflow variables -s DOMINO_USER_API_KEY $DOMINO_USER_API_KEY
 #start airflow webserver and scheduler
 echo "Starting up Airflow"
 airflow webserver -p 8080 -hn "0.0.0.0" &
