1e336b59ed36c7d8a414b076a901848ddee6b3ab
Justin Phelps <justin.phelps@dominodatalab.com>
2021-01-12 14:46:26 -0700

    added run start to save more time

--- run_start.sh
+++ run_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 curl https://raw.githubusercontent.com/Jphelps87/AirflowWorkspace/main/start.sh --output /home/ubuntu/airflowstart.sh
-sudo chmod 777 start.sh
-sh start.sh
+sudo chmod 777 /home/ubuntu/airflow/start.sh
+sh ./home/ubuntu/airflow/start.sh
 
