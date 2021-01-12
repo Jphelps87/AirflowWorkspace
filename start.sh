e1e692eea70ed0c272678b4efa2090ee281a10cb
Justin Phelps <justin.phelps@dominodatalab.com>
2021-01-12 15:59:10 -0700

    Tue Jan 12 15:59:10 MST 2021

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "start.sh script running"
+echo "Just a test"
 #Check if Airflow Dir is present in Domino Project....if not create airflow directory
 if [ ! -d $DOMINO_WORKING_DIR/airflow ]; then
 	echo "Creating Airflow Directory"
