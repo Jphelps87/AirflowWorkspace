a0ba508497ff8c6df20e329bc3191e46517a26fa
Justin Phelps <justin.phelps@dominodatalab.com>
2021-01-13 13:45:26 -0700

    Wed Jan 13 13:45:26 MST 2021

--- start.sh
+++ start.sh
@@ -51,10 +51,14 @@ if [ ! -d $DOMINO_WORKING_DIR/airflow ]; then
 git_cmd = /usr/bin/git
 git_default_args = -c color.ui=true
 git_init_repo = True
-root_directory = '"$DOMINO_WORKING_DIR"'/airflow/dags'
+root_directory = '"$DOMINO_WORKING_DIR"'/airflow/dags
+mount_name = data
+mount_path = /home/ubuntu/airflow/data
+mount1_name = logs
+mount1_path = '"$DOMINO_WORKING_DIR"'/airflow/logs'
 	echo "$editor_config" >> "$DOMINO_WORKING_DIR"/airflow/airflow.cfg
 
-	# #add demo DAGS
+	#add demo DAGS
 	# curl https://raw.githubusercontent.com/Jphelps87/AirflowWorkspace/main/domino-pipeline.py --output "$DOMINO_WORKING_DIR"/airflow/dags/domino-pipeline.py
 	# curl https://raw.githubusercontent.com/Jphelps87/AirflowWorkspace/main/hello_world.py --output "$DOMINO_WORKING_DIR"/airflow/dags/hello_world.py
 	# sudo chmod 777 -R "$DOMINO_WORKING_DIR"/airflow/dags/
